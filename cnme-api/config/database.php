5c40ffde6d4b64f77f947c6868a158e56e3f4155
Thiago Oliveira <thiagok2@gmail.com>
2019-01-11 20:15:55 -0300

    config postgresql

--- cnme-api/config/database.php
+++ cnme-api/config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+// $url = parse_url(getenv("DATABASE_URL"));
+
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -13,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,9 +67,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'cnme'),
+            'username' => env('DB_USERNAME', 'postgre'),
+            'password' => env('DB_PASSWORD', 'postgre'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
