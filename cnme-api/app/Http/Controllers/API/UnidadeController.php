da2024116290ef77564d181714c245db27ec90a8
Thiago Oliveira <thiagok2@gmail.com>
2019-01-15 16:36:42 -0300

    API UnidadeControler

--- cnme-api/app/Http/Controllers/API/UnidadeController.php
+++ cnme-api/app/Http/Controllers/API/UnidadeController.php
@@ -4,36 +4,60 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UnidadeResource;
+use App\Models\Unidade;
 
 class UnidadeController extends Controller
 {
    
+    
+
     public function index()
     {
-        return 'index';
+        return UnidadeResource::collection(Unidade::paginate(25));
     }
 
     
     public function store(Request $request)
     {
-        return 'store';
+
+        $unidade = $request->has('id') ? Unidade::findOrFail($request->id) : new Unidade;
+
+        $unidadeData = $request->all();
+        $unidade->fill($unidadeData);
+		$unidade->save();
+        
+        return new UnidadeResource($unidade);
     }
 
    
     public function show($id)
     {
-        return 'show';
+        return new UnidadeResource(Unidade::find($id));
     }
 
     
     public function update(Request $request, $id)
     {
-        return 'update';
+        $unidade = Unidade::findOrFail($id);
+        $unidadeData = $request->all();
+        $unidade->fill($unidadeData);
+        $unidade->save();
+
+
+        return new UnidadeResource($unidade);
     }
 
     
     public function destroy($id)
     {
-        return 'destroy';
+        $unidade = Unidade::find($id);
+        if(isset($unidade)){
+            $unidade->delete();
+            return '204';
+        }
+
+        return response('Unidade não encontrada.', 404);
+        
     }
 }
