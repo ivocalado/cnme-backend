f7302d02931986c5c36be5f7752ce7655fdee7c3
Thiago Oliveira <thiagok2@gmail.com>
2019-01-20 23:32:36 -0300

    Projeto / Solicitacao

--- cnme-api/app/Models/ProjetoCnme.php
+++ cnme-api/app/Models/ProjetoCnme.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjetoCnme extends Model
 {
-    //
+    protected $fillable = [
+        'id','numero', 'status','descricao','unidade_id','usuario_id','solicitacao_cnme_id','data_criacao'
+        ,'data_implantacao_prevista','data_implantacao_realizada','data_inicio_entrega'
+    ];
 }
--- cnme-api/app/Models/SolicitacaoCnme.php
+++ cnme-api/app/Models/SolicitacaoCnme.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class SolicitacaoCnme extends Model
 {
-    //
+    protected $fillable = [
+        'id','descricao', 'usuario_id','unidade_id','data_solicitacao'
+    ];
+
+    public function usuario(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function unidade(){
+        return $this->belongsTo(Unidade::class);
+    }
+
+    public $rules = [
+        'descricao'    =>  'required',
+        'data_solicitacao'       =>  'required|date',
+        'usuario_id' => 'required|integer',
+        'unidade_id' => 'required|integer'
+       
+    ];
+
+    public $messages = [
+        'required' => 'O campo :attribute é obrigatório',
+        'integer' => 'O campo :attribute deve ser um inteiro',
+        'date' => 'O campo :attribute é um campo no formato de data'
+    ];
 }
