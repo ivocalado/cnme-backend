b1d917292cb77c4da41789cd0126fdc4fed9c1fd
Thiago Oliveira <thiagok2@gmail.com>
2019-03-05 13:50:05 -0300

    add email use resource

--- cnme-api/app/Http/Resources/UserResource.php
+++ cnme-api/app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'nome' => $this->name,
             'name' => $this->name,
+            'email' => $this->email,
             'token' => $this->remember_token,
             'email_verified_at' => $this->email_verified_at,
             'ativo' => $this->email_verified_at !== null,
